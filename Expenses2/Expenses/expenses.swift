2e153feae570e476e884f3d67de10598ed32e91e
bl223 <35577366+bl223@users.noreply.github.com>
2019-06-20 19:39:47 -0400

    commit2

---
+++ Expenses2/Expenses/expenses.swift
@@ -0,0 +1,17 @@
+//
+//  expenses.swift
+//  Expenses
+//
+//  Created by Bl223LabCPU on 6/7/19.
+//  Copyright © 2019 Bl223LabCPU. All rights reserved.
+//
+
+import Foundation
+
+struct expense {
+    
+    let title: String
+    let amount: Double
+    let date: Date
+    
+}
